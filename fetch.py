2832270794a62529b6d281b3c27ff3d8a5aac0bd
DESKTOP-LB80IEP\Awal <awalmalhotra@gmail.com>
2018-02-18 17:06:59 +0530

    no message

--- fetch.py
+++ fetch.py
@@ -7,10 +7,10 @@ Created on Tue Feb  6 16:58:36 2018
 
 import tweepy
 from tweepy import OAuthHandler
-consumer_key = 'PwP7TRcyZfl4SnqEBVJSMKa6w'
-consumer_secret = '3B6NnACsu22xf78BnJNVssOittaN8c0XNbVZMfyppgq9FbStV0'
-access_token = '622721291-lAeWJ46G9T8sVNoCRjngZFkzAABLnnkYV6UktVmq'
-access_secret = 'b42UAgFQ503Wqe8CdO7pX6jb39nkx0hOdgXeoCbJp0tF3'
+consumer_key = '############'
+consumer_secret = '##################'
+access_token = '########################'
+access_secret = '###############################'
  
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
