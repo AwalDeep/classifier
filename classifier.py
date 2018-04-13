7ad6334cc78dcdacec4d0193b536a053f581812c
AwalDeep <awalmalhotra@gmail.com>
2018-04-13 23:33:06 +0530

    pie chart

--- classifier.py
+++ classifier.py
@@ -3,9 +3,9 @@
 Created on Thu Feb 22 13:37:36 2018
 
 @author: Awal
+"""
 
-
-""""IMPORTING LIBRARIES AND HEADER FILES """
+#     IMPORTING LIBRARIES AND HEADER FILES
 
 
 import tweepy
@@ -14,13 +14,17 @@ import numpy
 from textblob import TextBlob
 import csv
 import re
+import pandas as pd
+#import matplotlib
+import matplotlib.pyplot as plt
+#import pickle
 
-""""AUTHENTICATION OF TWITTER API """
+#AUTHENTICATION OF TWITTER API 
 
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_secret = ''
+consumer_key = 'PwP7TRcyZfl4SnqEBVJSMKa6w'
+consumer_secret = '3B6NnACsu22xf78BnJNVssOittaN8c0XNbVZMfyppgq9FbStV0'
+access_token = '622721291-lAeWJ46G9T8sVNoCRjngZFkzAABLnnkYV6UktVmq'
+access_secret = 'b42UAgFQ503Wqe8CdO7pX6jb39nkx0hOdgXeoCbJp0tF3'
  
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
@@ -28,7 +32,8 @@ auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 
 
-""""GETTING INPUT FROM USER AND FETCHING TWEETS ACCORDINGLY"""
+
+#   GETTING INPUT FROM USER AND FETCHING TWEETS ACCORDINGLY
 
 
 twt=[]
@@ -50,7 +55,7 @@ if(choice==2):
 if(choice==3):
     results=tweepy.Cursor(api.user_timeline,tweet_mode='extended').items(100)
     
-""""PROCESSING TWEETS AND STORING IT IN A CSV FILE """
+#PROCESSING TWEETS AND STORING IT IN A CSV FILE
 
 
 for status in results:
@@ -58,34 +63,43 @@ for status in results:
     a=((status.full_text).encode("utf-8"))
     twt.append(a)    
     
-data=open('tweet.csv','w')
-out= csv.writer(data) 
-out.writerow(twt)      
-data.close()
+
 polar=numpy.empty(100,dtype=float)
+result=numpy.empty(100,dtype="S10")
+
+with open('tweet.csv', "w") as data:
+    out = csv.writer(data, lineterminator='\n')
+    for val in twt:
+        out.writerow([val])    
 
 
-""""USING TEXTBLOB LIBRARY TO FIND SENTIMENTS AND PRINT THE OUTPUT """
+
+
+""""USING TEXTBLOB LIBRARY TO FIND SENTIMENTS AND GET THE OUTPUT """
 
 
 i=0
-countp=countn=0
+countp=countn=countnt=0
 with open('tweet.csv', 'r') as csvfile:
     rows = csv.reader(csvfile)
-
-    for row in rows:
-        for k in row:
-            sentence = k
-            sentence = re.sub(r'https?:\/\/.*[\r\n]*', '', sentence)
-            blob = TextBlob(sentence)
-            polar[i]=blob.sentiment.polarity
-            i=i+1 
-            if(blob.sentiment.polarity>0):
-                    print("Happy :)")
-                    countp+=1
-            else:
-                print("Sad :(")
-                countn +=1
+    for k in rows:
+        sentence=re.sub(r'https?:\/\/.*[\r\n]*', '', str(k))
+        blob = TextBlob(sentence)
+        polar[i]=blob.sentiment.polarity
+         
+        if(blob.sentiment.polarity>0):
+            expression='Positive';        
+                
+            countp+=1
+        elif(blob.sentiment.polarity<0) :
+            expression='Negative';
+            countn +=1
+        else:
+            expression='Neutral';
+            countnt +=1
+        print(expression)    
+        result[i]=expression
+        i=i+1
                 
             # -*- coding: utf-8 -*-
 print("Positive tweets: %s"%countp)
@@ -94,3 +108,32 @@ if(countn>=2*countp):
     print("Negative Response") 
 if(countp>=2*countn):
     print("Positive Response")
+    
+        
+df = pd.DataFrame({"Tweet":twt,"Polarity" : polar, "Sentiment" :result })
+df.to_csv("sentiment.csv", index=False)        
+    
+
+with open('sentiment.csv', 'r', encoding = 'utf8') as sencsvfile:
+        # Pandas to read the “Sentiment” column,
+        df_sen = pd.read_csv(sencsvfile)
+        sent_tweet = df_sen["Sentiment"]
+
+#use Counter to count how many times each sentiment appears and save each as a variable
+        #counter_var = Counter(sent_tweet)
+Positive = countp
+Negative = countn
+Neutral = countnt        
+
+## declare the variables for the pie chart, using the Counter variables for “sizes”
+labels = 'Positive', 'Negative', 'Neutral'
+sizes = [Positive, Negative, Neutral]
+colors = ['green', 'red', 'yellow']
+text = query
+
+## use matplotlib to plot the chart
+plt.pie(sizes, labels = labels, colors = colors, shadow = True, startangle = 90)
+plt.title("Sentiment of 100 Tweets about "+ text)
+plt.show()
+
+    
